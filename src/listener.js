1a8f527a60a2ec064a769ea76c54d2f7b190fc66
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-28 19:04:02 +0700

    update listener.js

--- src/listener.js
+++ src/listener.js
@@ -7,11 +7,11 @@ class Listener {
     }
 
     async listen(message) {
-      const { userId, targetEmail } = JSON.parse(message.content.toString());
-      const notes = await this._notesService.getNotes(userId);
-      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(notes));
-      console.log(result);
       try {
+        const { userId, targetEmail } = JSON.parse(message.content.toString());
+        const notes = await this._notesService.getNotes(userId);
+        const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(notes));
+        console.log(result);
       } catch (error) {
         console.error(error);
       }
